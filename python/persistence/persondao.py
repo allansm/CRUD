dd3f15d28fd7fbbdaafbbffae3c027c20c9cb5e0
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-03-02 16:40:16 -0300

    Add read method.

--- python/persistence/persondao.py
+++ python/persistence/persondao.py
@@ -16,3 +16,29 @@ class PersonDao:
 
         stmt.close()
         connection.close()
+
+    def read(self, connection):
+        from entity.person import Person
+
+        stmt = connection.cursor()
+
+        sql = "SELECT * FROM person"
+
+        stmt.execute(sql)
+        results = stmt.fetchall()
+
+        stmt.close()
+        connection.close()
+        
+        list = []
+        for result in results:
+            person = Person()
+            
+            person.setId(result[0])
+            person.setFirstName(result[1])
+            person.setLastName(result[2])
+            person.setEmail(result[3])
+            
+            list.append(person)
+        
+        return list
--- python/test.py
+++ python/test.py
@@ -27,5 +27,17 @@ def create():
     except:
         print("fail")
 
+def read():
+    from connection.pythoncrud import PythonCRUD
+    from persistence.persondao import PersonDao
+
+    list = PersonDao().read(PythonCRUD().getConnection())
+    for n in list:
+        print("id:"+str(n.getId()))
+        print("first name:"+n.getFirstName())
+        print("last name:"+n.getLastName())
+        print("email:"+n.getEmail()+"\n")
+
 connection()
 create()
+read()
